8cf749cec170c4a898d7ff6d1fef296a76e19fd4
leonardo bulcão <leobc009@gmail.com>
2021-09-29 15:49:13 -0300

    propriedades adicionadas

--- filme.py
+++ filme.py
@@ -1,47 +1,37 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self.__nome = nome
+        self.ano = ano
+        self.__like = 0
+
+    def dar_like(self):
+        self.__like += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novoNome):
+        self.__nome = novoNome
+
+    @property
+    def like(self):
+        return self.__like
+
+    def dar_like(self):
+        self.__like += 1
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__like = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self,nome):
-        self.__nome = nome
-
-    @property
-    def like(self):
-        return self.__like
-
-    def dar_like(self):
-        self.__like += 1
 
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__like = 0
 
-    def dar_like(self):
-        self.__like += 1
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-
-    @property
-    def like(self):
-        return self.__like
-
-    def dar_like(self):
-        self.__like += 1
